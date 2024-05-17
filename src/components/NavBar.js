5ebfe4f4711d12fc333cb6e5902c9748c181f7d8
Joy Li <joy0yt0li@gmail.com>
2024-05-17 13:02:31 -0700

    nav bar updates

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,60 +1,102 @@
-import React from 'react';
+import React, {useState} from 'react';
+// temporary, move to state
+const navItems = [{ current: true, linkTo: "index.html", text: "Home" },
+{ current: false, linkTo: "mycloset.html", text: "My Closet" },
+{ current: false, linkTo: "profile.html", text: "Profile" }];
 
 export default function NavBar(props) {
+    const [showMobileMenu, toggleMobile] = useState(false);
+
+    const toggleMobileMenu = function(event) {
+        toggleMobile(!showMobileMenu);
+        console.log("hello!!");
+    }
+
     return (
         <header role="banner">
             <div className="menu-bar">
                 <div className="logo">
                     <a href="index.html"><img src="icon/logo 2.svg" alt="closetify" /></a>
                 </div>
-                <div className="icon-group">
-                    <nav className="icon-menu" role="navigation">
-                        <a href="#mobileNavDropdown">
-                            <img className="icon-svg" src="icon/hamburger.svg" alt="menu" />
-                        </a>
-                    </nav>
-                </div>
-                <nav className="menu-list-desktop" role="navigation">
-                    <div className="nav-item nav-current">
-                        <img src="icon/pin.svg" alt="pin" />
-                            <a href="index.html">Home</a>
-                    </div>
-                    <div className="nav-item">
-                        <a href="mycloset.html">My Closet</a>
-                    </div>
-                    <div className="nav-item">
-                        <a href="profile.html">Profile</a>
-                    </div>
-                </nav>
+                <MobileNav toggleMobileMenu={toggleMobileMenu}/>
+                <DesktopNav />
             </div>
-            <div id="mobileNavDropdown" className="dropdown-content">
-                <div className="menu-list-mobile">
-                    <div className="menu-list-mobile-pin">
-                        <img src="icon/pin.svg" alt="pin" />
-                    </div>
-                    <a href="index.html">
-                        <h1>Home</h1>
-                    </a>
-                </div>
-                <div className="menu-list-mobile">
-                    <div className="menu-list-mobile-pin">
-                    </div>
-                    <a href="mycloset.html">
-                        <h1>My Closet</h1>
-                    </a>
-
-                </div>
-
-                <div className="menu-list-mobile">
-                    <div className="menu-list-mobile-pin">
-                    </div>
-                    <a href="profile.html">
-                        <h1>Profile</h1>
-                    </a>
-
-                </div>
-            </div>
-
+            {showMobileMenu && <MobileDropdownMenu />}
         </header>
+    );
+}
+
+// DESKTOP NAV
+// TODO: change <a> to <link> after routing
+function DesktopNavItem({ current, linkTo, text }) {
+    // current is a boolean
+    return (
+        <div className={current ? "nav-item nav-current" : "nav-item"}>
+            {current && <img src="icon/pin.svg" alt="pin" />}
+            <a href={linkTo}>{text}</a>
+        </div>
+    );
+}
+
+function DesktopNav(props) {
+    // TODO: pass down navitems as state variable so that "current" is updated onClick
+
+    const navArray = navItems.map((item) => (
+        <DesktopNavItem key={item.text} current={item.current} linkTo={item.linkTo} text={item.text} />
+    ));
+
+    return (
+        <nav className="menu-list-desktop" role="navigation">
+            <div className="nav-item nav-current">
+                <img src="icon/pin.svg" alt="pin" />
+                <a href="index.html">Home</a>
+            </div>
+            <div className="nav-item">
+                <a href="mycloset.html">My Closet</a>
+            </div>
+            <div className="nav-item">
+                <a href="profile.html">Profile</a>
+            </div>
+        </nav>
+    );
+}
+
+// MOBILE NAV
+
+// TODO: fix dropdown behavior to be interactive
+function MobileNav(props) {
+    return (
+        <div className="icon-group" onClick={props.toggleMobileMenu}>
+            <nav className="icon-menu" role="navigation">
+                <a href="#mobileNavDropdown">
+                    <img className="icon-svg" src="icon/hamburger.svg" alt="menu" />
+                </a>
+            </nav>
+        </div>
+    );
+}
+
+function MobileNavItem({ current, linkTo, text }) {
+    // current is a boolean
+    return (
+        <div className="menu-list-mobile">
+            <div className="menu-list-mobile-pin">
+                {current && <img src="icon/pin.svg" alt="pin" />}
+            </div>
+            <a href={linkTo}>
+                <h1>{text}</h1>
+            </a>
+        </div>
+    );
+}
+
+function MobileDropdownMenu(props) {
+    const navArray = navItems.map((item) => (
+        <MobileNavItem key={item.text} current={item.current} linkTo={item.linkTo} text={item.text} />
+    ));
+    return (
+        <div id="mobileNavDropdown" className="dropdown-content">
+            {navArray}
+        </div>
     );
 }
