ded8687b8466e6d524542ba0b09081edec2f5c51
junemih <junehong@uw.edu>
2024-05-31 15:52:57 -0700

    cleanup

--- src/components/SignInPage.js
+++ src/components/SignInPage.js
@@ -2,14 +2,13 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { auth, googleProvider, emailProvider } from '../firebase.js';
 
-//import { getAuth, GoogleAuthProvider, EmailAuthProvider } from 'firebase/auth';
 import StyledFirebaseAuth from './StyledFirebaseAuth';
 
 const firebaseUIConfig = {
   signInOptions: [
     googleProvider.providerId,
     {
-      provider: emailProvider.providerId, //EmailAuthProvider.PROVIDER_ID,
+      provider: emailProvider.providerId,
       requireDisplayName: true,
     },
   ],
@@ -23,14 +22,11 @@ const firebaseUIConfig = {
 };
 
 const SignInPage = () => {
-  //const auth = getAuth(); // Access the authenticator
-
   const navigate = useNavigate();
 
   firebaseUIConfig.callbacks.signInSuccessWithAuthResult = () => {
     navigate("/home");
   };
-
 
   return (
     <div className="sign-in-page">
